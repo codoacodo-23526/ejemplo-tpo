51f5fa7d37bdfc2e5d34ec69ffa09ed3987cfbef
german.settino <german.settino@menta.global>
2023-09-20 20:31:14 -0300

    javascript

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+console.log('Hello World!');
+
+// Declarar una variable 
+//Nombre de la variable no permite espacios
+/* var nombre Variable;
+var nombreVariable;
+*/
+
+// Buena practica camelCase
+var nombreVariable;
+let nombreVariable2;
+
+//snake_case
+var nombre_variable;
+let nombre_variable2;
+
+
+nombreVariable = "Hola Mundo";
+
+
+const email = "german@gmail.com";
+
+//email = "pablo@gmail.com";
+
+
+
+if (true) {
+    let nombre = "German";
+    console.log(nombre); // German
+}
+
+console.log(nombre); // undefined
+
+
+if (true) {
+    var nombre = "German";
+    console.log(nombre);
+}
+
+console.log(nombre); // German
